c8a5886120845cb77700a5c1ba232dc606eb3ba2
prasa <prasa@DESKTOP-5U96D3H>
2022-09-04 09:52:16 +0530

    First Commit___

---
+++ src/com/ex/io/Test.java
@@ -0,0 +1,21 @@
+package com.ex.io;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String Call="Calling";
+		
+		if(Call=="Calling") {
+			 System.out.println("Hello May i know who are you?");
+			
+		}
+		else {
+			
+			System.out.println("He/Her not lifting call");
+		}
+
+	}
+
+}
